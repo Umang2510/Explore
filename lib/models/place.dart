aaf9d6170a62d8bb18e104a82edbf6e75a294a31
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2023-01-16 14:41:14 +0530

    sec-13 296.PlaceList & Place Provider setup

---
+++ lib/models/place.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String? address;
+
+  PlaceLocation(
+      {required this.latitude, required this.longitude, this.address});
+}
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image; //File data type buit in dart, To sotre file in a device
+
+  Place({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.image,
+  });
+}
